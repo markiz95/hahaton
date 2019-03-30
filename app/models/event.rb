899a72f52f4bcc3f56030358e92cea56067df0e0
dmitriytarashkevich <dmitriy.tarashkevich.95@mail.ru>
2019-03-30 20:23:04 +0300

    Cleaning

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   has_many :members
-  has_many :users, through: :members, dependent: :destroy
-  belongs_to :creator, foreign_key: :creator_id, class_name: "User", dependent: :destroy
+  has_many :users, through: :members
+  belongs_to :creator, foreign_key: :creator_id, class_name: "User"
   has_many :taggings
   has_many :tags, through: :taggings
 
