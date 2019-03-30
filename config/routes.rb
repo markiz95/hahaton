0b67963e983e07c50c27108fdc322fa3f2bae322
dmitriytarashkevich <dmitriy.tarashkevich.95@mail.ru>
2019-03-30 17:16:58 +0300

    2019 fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :events
   get 'events/:id/join', to: 'events#join', as: "join_event"
-  root 'welcome#index'
+  root 'events#index'
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
