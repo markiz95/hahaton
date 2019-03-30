3fa2e94ea0fd251f6d2ce4fbf2c52bdf81c3655e
dmitriytarashkevich <dmitriy.tarashkevich.95@mail.ru>
2019-03-30 18:35:21 +0300

    Tags added

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,14 @@ class Event < ApplicationRecord
   belongs_to :creator, foreign_key: :creator_id, class_name: "User", dependent: :destroy
   has_many :taggings
   has_many :tags, through: :taggings
+
+  def all_tags
+    self.tags.map(&:name).join(', ')
+  end
+
+  def all_tags=(names)
+    self.tags = names.split(',').map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
 end
