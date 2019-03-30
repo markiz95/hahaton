7707fab7118cf23d098974826f29c334f6e2dc87
dmitriytarashkevich <dmitriy.tarashkevich.95@mail.ru>
2019-03-30 20:37:37 +0300

    dependent destroy for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  has_many :members
+  has_many :members, dependent: :destroy
   has_many :users, through: :members
   belongs_to :creator, foreign_key: :creator_id, class_name: "User"
   has_many :taggings
