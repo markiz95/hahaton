33f92d576870e0425cc4361f791f743637701dd6
dmitriytarashkevich <dmitriy.tarashkevich.95@mail.ru>
2019-03-30 15:12:50 +0300

    Events fixed, tags added

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ApplicationRecord
   has_many :members
   has_many :users, through: :members
   belongs_to :creator, foreign_key: :creator_id, class_name: "User"
-    # has_many :taggings, dependent: :destroy
-    # has_many :tags, through: :taggings
+  has_many :taggings
+  has_many :tags, through: :taggings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_110757) do
+ActiveRecord::Schema.define(version: 2019_03_30_114514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,22 @@ ActiveRecord::Schema.define(version: 2019_03_30_110757) do
     t.index ["user_id"], name: "index_members_on_user_id"
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "event_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_taggings_on_event_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -55,8 +71,11 @@ ActiveRecord::Schema.define(version: 2019_03_30_110757) do
     t.datetime "updated_at", null: false
     t.string "uid"
     t.string "encrypted_password"
+    t.string "image"
   end
 
   add_foreign_key "members", "events"
   add_foreign_key "members", "users"
+  add_foreign_key "taggings", "events"
+  add_foreign_key "taggings", "tags"
 end
