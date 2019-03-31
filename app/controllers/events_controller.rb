7ee133f8dc1a03662af95f15ee0394233d908824
Vladislav Suvorov <vladmarkiz95@yandex.ru>
2019-03-31 09:41:12 +0300

    Fix layout

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,7 +7,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.includes(:tags, :members, :creator).all
   end
 
   # GET /events/1
