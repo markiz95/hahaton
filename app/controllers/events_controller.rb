a5112cff010d65b5978efa7ea63f21ddae6574de
dmitriytarashkevich <dmitriy.tarashkevich.95@mail.ru>
2019-03-31 12:37:36 +0300

    Join on create and update Event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -29,7 +29,7 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
     @event.creator = current_user
-    @event.users << current_user
+    join
 
     respond_to do |format|
       if @event.save
@@ -45,6 +45,7 @@ class EventsController < ApplicationController
   # PATCH/PUT /events/1
   # PATCH/PUT /events/1.json
   def update
+    join
     respond_to do |format|
       if @event.update(event_params)
         format.html { redirect_to @event, notice: 'Event was successfully updated.' }
