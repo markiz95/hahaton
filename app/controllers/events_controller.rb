0eeb58076baa6185d32d1279f51108afb1ffc6b5
dmitriytarashkevich <dmitriy.tarashkevich.95@mail.ru>
2019-03-31 10:49:29 +0300

    event_params fixed

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -112,7 +112,7 @@ class EventsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def event_params
-    params.require(:event).permit(:title, :description, :min_people, :max_people, :members, :creator, :start, :end, :image)
+    params.require(:event).permit(:title, :description, :min_people, :max_people, :all_tags, :creator, :start, :end, :image)
   end
 
   def google_secret
